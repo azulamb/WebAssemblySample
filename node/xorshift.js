15b2c63b538d23fb74038b072f5faea7b55bc195
Hiroki <1127785+azulamb@users.noreply.github.com>
2020-02-05 09:31:01 +0900

    Fix.

--- node/xorshift.js
+++ node/xorshift.js
@@ -1,3 +1,6 @@
 import * as xorshift from '../docs/xorshift.wasm'
 
 console.log( xorshift );
+console.log( '0x' + xorshift.nextInt().toString( 16 ) );
+console.log( '0x' + xorshift.nextInt().toString( 16 ) );
+console.log( '0x' + xorshift.nextInt().toString( 16 ) );
